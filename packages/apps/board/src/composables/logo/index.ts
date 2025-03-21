5e4459c9c35df6a680b320427b635651f3cd66bf
FrexCheat <frexcheat@163.com>
2025-03-21 22:26:00 +0800

    feat: add zzuli logo preset

--- packages/apps/board/src/composables/logo/index.ts
+++ packages/apps/board/src/composables/logo/index.ts
@@ -3,6 +3,7 @@ import type { Image } from "@xcpcio/types";
 import { GET_LOGO_CCPC } from "./ccpc";
 import { GET_LOGO_HUNAN_CPC } from "./hunan-cpc";
 import { GET_LOGO_ICPC } from "./icpc";
+import { GET_LOGO_ZZULI } from "./zzuli";
 
 export function getLogoFromPreset(image: Image): Image {
   if (!image.preset) {
@@ -17,6 +18,10 @@ export function getLogoFromPreset(image: Image): Image {
 
   if (preset === "CCPC") {
     return GET_LOGO_CCPC();
+  }
+
+  if (preset === "ZZULI") {
+    return GET_LOGO_ZZULI();
   }
 
   if (preset === "HUNAN_CPC") {
