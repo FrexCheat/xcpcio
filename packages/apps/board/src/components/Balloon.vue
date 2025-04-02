0ff07ac1bd8e0b80bd72458abf66452ce7b6d211
FrexCheat <frexcheat@163.com>
2025-04-02 16:35:40 +0800

    feat: add balloons location filter

--- packages/apps/board/src/components/Balloon.vue
+++ packages/apps/board/src/components/Balloon.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
-import type { Contest, Submissions, Teams } from "@xcpcio/core";
+import type { Contest, SelectOptionItem, Submissions, Teams } from "@xcpcio/core";
 import type { Contest as IContest, Submissions as ISubmissions, Teams as ITeams } from "@xcpcio/types";
 import { Balloon, createContest, createSubmissions, createTeams, Rank } from "@xcpcio/core";
+
+import { MultiSelect } from "vue-search-select";
 
 import { BALLOON_TITLE_SUFFIX } from "~/composables/constant";
 
@@ -18,6 +20,20 @@ const teamsData = ref([] as Teams);
 const submissionsData = ref([] as Submissions);
 const rank = ref({} as Rank);
 const now = useNow();
+
+const locationOptions = computed(() => {
+  return [...new Set(
+    teamsData.value.filter(t => t.location).map(t => t.location),
+  )].map(o => ({ value: o, text: o }));
+});
+
+const locationSelectedItems = ref<Array<SelectOptionItem>>([]);
+const locationLastSelectItem = ref({});
+
+function locationOnSelect(selectedItems: Array<SelectOptionItem>, lastSelectItem: SelectOptionItem) {
+  locationSelectedItems.value = selectedItems;
+  locationLastSelectItem.value = lastSelectItem;
+}
 
 function reBuildBalloons() {
   const newRank = new Rank(contestData.value, teamsData.value, submissionsData.value);
@@ -43,8 +59,12 @@ watch(data, async () => {
 }, { immediate: true });
 
 const balloons = computed(() => {
-  // eslint-disable-next-line vue/no-side-effects-in-computed-properties
-  return rank.value.balloons.sort(Balloon.compare).reverse();
+  return rank.value.balloons.filter((b) => {
+    if (locationSelectedItems.value.length === 0) {
+      return true;
+    }
+    return locationSelectedItems.value.some(o => o.value === b.team.location);
+  }).sort(Balloon.compare).reverse();
 });
 </script>
 
@@ -78,6 +98,32 @@ const balloons = computed(() => {
         flex flex-col
         justify-between
       >
+        <div
+          sticky top-0
+          w-full h-15
+          gap-3
+          flex flex-row
+          items-center
+          bg-coolGray-600
+          bg-opacity-80
+        >
+          <div
+            ml-2
+            text-size-xl
+          >
+            {{ t("balloons.location_filter") }}
+          </div>
+          <div
+            w-690px
+          >
+            <MultiSelect
+              :options="locationOptions"
+              :selected-options="locationSelectedItems"
+              placeholder="Location"
+              @select="locationOnSelect"
+            />
+          </div>
+        </div>
         <template
           v-for="(b, index) in balloons"
           :key="b.key"
